eeba61b59ae7b8182ee00b8526d9400a6af8eee4
kokhno-nikolay <kokhno.root@gmail.com>
2022-04-18 14:11:29 +0300

    added some debug log

--- api/chat.go
+++ api/chat.go
@@ -48,6 +48,8 @@ func (h *Handler) handleConnections(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) messageClient(client *websocket.Conn, msg models.ChatMessage) {
+	log.Println("MESSAGE: ", msg)
+
 	err := client.WriteJSON(msg)
 	if err != nil && h.unsafeError(err) {
 		log.Printf("error: %v", err)
@@ -58,6 +60,8 @@ func (h *Handler) messageClient(client *websocket.Conn, msg models.ChatMessage) 
 
 func (h *Handler) messageClients(msg models.ChatMessage) {
 	for client := range h.clients {
+		log.Println("CLIENTS MESSAGE: ", msg)
+
 		h.messageClient(client, msg)
 	}
 }
